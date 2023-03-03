41fff5c9906d435886d5dc8ac4b84c6a67432077
Typical-User0 <fffuwiwo2j3nt@gmail.com>
2023-03-03 20:19:42 +0300

    Added comments into hello_world.cpp for clarity

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,10 +5,13 @@
 
 
 int main () {
+    // user's name
     std::string name;
 
+    // read user's name from stdin 
     std::cin >> name;
 
+    // greeting from user
     std::cout << "Hello world from " << name << std::endl;
     return 0; 
 }
