e7ff613ac7ba871d3e9be7144d885bb9555df718
Typical-User0 <fffuwiwo2j3nt@gmail.com>
2023-03-03 20:12:34 +0300

    Removed 'using namespace std' from hello_world.cpp file

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,14 +2,13 @@
 #include <string>
 
 
-using namespace std
 
 
 int main () {
-    string name;
+    std::string name;
 
-    cin >> name;
+    std::cin >> name;
 
-    cout << "Hello world from " << name << endl;
+    std::cout << "Hello world from " << name << std::endl;
     return 0; 
 }
